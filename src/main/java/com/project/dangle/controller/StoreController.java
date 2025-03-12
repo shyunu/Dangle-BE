d2f92e1157935099835f123ba0fa7e44ecde22d2
suhyun-kim9 <ksh020674@gmail.com>
2025-03-12 18:37:32 +0900

    StoreController수정

--- src/main/java/com/project/dangle/controller/StoreController.java
+++ src/main/java/com/project/dangle/controller/StoreController.java
@@ -4,15 +4,13 @@ import com.project.dangle.command.DesignerVO;
 import com.project.dangle.command.StoreVO;
 import com.project.dangle.store.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/store")
 public class StoreController {
 
     @Autowired
