55a17b5a3e3958e20753729b5ec61b740246a5f2
suhyun-kim9 <ksh020674@gmail.com>
2025-03-16 17:22:58 +0900

    searchAccount-option변경

--- src/main/frontend/src/screens/account/SearchAccount.tsx
+++ src/main/frontend/src/screens/account/SearchAccount.tsx
@@ -3,19 +3,29 @@ import "../../styles/account/SearchAccount.css";
 import Button from "../../components/Button";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { IoMdRadioButtonOff, IoMdRadioButtonOn } from "react-icons/io";
 
 const SearchAccount: React.FC = () => {
     const navigation = useNavigate();
     const location = useLocation(); // 넘긴 state값 가져올때 사용
     const [isFindId, setIsFindId] = useState(true);
+    const [selectedOption, setSelectedOption] = useState<number>(0);
 
-    const [selectedOption, setSelectedOption] = useState<number | null>(null);
-    const changeOption = (e: number) => {
-        setSelectedOption(e);
-    };
-
+    const [userEmail, setUserEmail] = useState("");
+    const [userName, setUserName] = useState("");
     const [userId, setUserId] = useState("");
     const [userPhone, setUserPhone] = useState("");
+
+    const changeOption = (option: number) => {
+        setSelectedOption(option);
+
+        if (option === 1) {
+            setUserName("");
+            setUserPhone("");
+        } else if (option === 2) {
+            setUserEmail("");
+        }
+    };
 
     const handleNavigation = async () => {
         if (isFindId) {
@@ -24,14 +34,14 @@ const SearchAccount: React.FC = () => {
             try {
                 const response = await axios.post("/account/findAccountForPw", { userId, userPhone });
 
-                if(response.data === "계정확인완료") {
-                    navigation("/resetPw", { state : { userId, userPhone } });
+                if (response.data === "계정확인완료") {
+                    navigation("/resetPw", { state: { userId, userPhone } });
                 } else {
                     alert(response.data);
                 }
             } catch (error) {
                 console.error("에러발생:", error);
-                alert("비밀번호찾기 서버연결에 문제 발생함")
+                alert("비밀번호찾기 서버연결에 문제 발생함");
             }
         }
     };
@@ -62,42 +72,58 @@ const SearchAccount: React.FC = () => {
                 {isFindId ? (
                     <>
                         <div className="emailContainer">
-                            <div className="radioWrap">
-                                <input
-                                    type="radio"
-                                    id="option1"
-                                    name="searchOption"
-                                    checked={selectedOption === 1}
-                                    onChange={() => changeOption(1)}
-                                />
+                            <div className="radioWrap" onClick={() => changeOption(1)}>
+                                {selectedOption === 1 ? <IoMdRadioButtonOn /> : <IoMdRadioButtonOff />}
                                 <p>가입한 이메일로 찾기</p>
                             </div>
-                            <input placeholder="이메일을 입력해주세요." />
+                            <input
+                                type="email"
+                                value={userEmail}
+                                onChange={(e) => setUserEmail(e.target.value)}
+                                placeholder="이메일을 입력해주세요."
+                                disabled={selectedOption === 2}
+                            />
                         </div>
                         <div className="phoneContainer">
-                            <div className="radioWrap">
-                                <input
-                                    type="radio"
-                                    id="option2"
-                                    name="searchOption"
-                                    checked={selectedOption === 2}
-                                    onChange={() => changeOption(2)}
-                                />
+                            <div className="radioWrap" onClick={() => changeOption(2)}>
+                                {selectedOption === 2 ? <IoMdRadioButtonOn /> : <IoMdRadioButtonOff />}
                                 <p>가입한 휴대폰으로 찾기</p>
                             </div>
                             <p>이름</p>
-                            <input placeholder="이름을 입력해주세요." />
+                            <input
+                                type="text"
+                                value={userName}
+                                onChange={(e) => setUserName(e.target.value)}
+                                placeholder="이름을 입력해주세요."
+                                disabled={selectedOption === 1}
+                            />
                             <p>전화번호</p>
-                            <input placeholder="전화번호를 입력해주세요." />
+                            <input
+                                type="text"
+                                value={userPhone}
+                                onChange={(e) => setUserPhone(e.target.value)}
+                                placeholder="전화번호를 입력해주세요."
+                                disabled={selectedOption === 1}
+                            />
                         </div>
                     </>
                 ) : (
                     <>
                         <div className="pwContainer">
                             <p>아이디</p>
-                            <input type="text" placeholder="아이디를 입력해주세요." value={userId} onChange={(e) => setUserId(e.target.value)} />
+                            <input
+                                type="text"
+                                placeholder="아이디를 입력해주세요."
+                                value={userId}
+                                onChange={(e) => setUserId(e.target.value)}
+                            />
                             <p>전화번호</p>
-                            <input type="text" placeholder="전화번호를 입력해주세요." value={userPhone} onChange={(e) => setUserPhone(e.target.value)} />
+                            <input
+                                type="text"
+                                placeholder="전화번호를 입력해주세요."
+                                value={userPhone}
+                                onChange={(e) => setUserPhone(e.target.value)}
+                            />
                         </div>
                     </>
                 )}
